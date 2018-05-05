29f367067466a48889d7ac90c9f9b08a4608f84f
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-05-05 11:58:13 -0500

    mapping bidirectional entities
    mapping bidirectional entities

--- src/main/java/mx/infotec/dads/kukulkan/engine/translator/dsl/RelationshipMapping.java
+++ src/main/java/mx/infotec/dads/kukulkan/engine/translator/dsl/RelationshipMapping.java
@@ -1,8 +1,9 @@
 package mx.infotec.dads.kukulkan.engine.translator.dsl;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
+
+import org.apache.metamodel.MetaModelException;
 
 import mx.infotec.dads.kukulkan.metamodel.foundation.Entity;
 import mx.infotec.dads.kukulkan.metamodel.foundation.EntityAssociation;
@@ -21,17 +22,32 @@ public class RelationshipMapping {
 
     public static List<Entity> mapRelationships(VisitorContext vctx) {
         List<Entity> entities = vctx.getElements();
-        List<Association> associations = vctx.getAssociations();
-        associations.parallelStream().map(association -> toEntityAssociation(association, entities))
-                .collect(Collectors.toList());
+        vctx.getAssociations().stream().forEach(association -> mapAssociationToEntities(association, entities));
         return entities;
     }
 
-    public static EntityAssociation toEntityAssociation(Association association, List<Entity> entities) {
+    public static EntityAssociation mapAssociationToEntities(Association association, List<Entity> entities) {
         EntityAssociation ea = new EntityAssociation();
+        Entity entitySource = findEntityInList(association.getSource(), entities);
+        Entity entityTarget = findEntityInList(association.getTarget(), entities);
+        ea.setSource(entitySource);
+        ea.setTarget(entityTarget);
+        ea.setType(association.getType());
         ea.setSourcePropertyName(association.getSourcePropertyName());
         ea.setTargetPropertyName(association.getTargetPropertyName());
+        entitySource.getAssociations().add(ea);
+        entityTarget.getAssociations().add(ea);
         return ea;
+    }
+
+    public static Entity findEntityInList(String entityName, List<Entity> entities) {
+        for (Entity entity : entities) {
+            if (entity.getName().equals(entityName)) {
+                return entity;
+            }
+        }
+        throw new MetaModelException("entity name: [" + entityName
+                + "] have not been founded in the Entity List, this name must match some value in the List ");
     }
 
 }
