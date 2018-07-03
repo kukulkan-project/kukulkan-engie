e35b0efcb343c17511f758d4a75387cc7cbda868
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-07-02 19:22:23 -0500

    Adding support for Relational database
    Adding support for Relational database

---
+++ src/main/java/mx/infotec/dads/kukulkan/engine/translator/database/DataBaseTranslatorService.java
@@ -0,0 +1,22 @@
+package mx.infotec.dads.kukulkan.engine.translator.database;
+
+import mx.infotec.dads.kukulkan.engine.translator.Source;
+import mx.infotec.dads.kukulkan.engine.translator.TranslatorService;
+import mx.infotec.dads.kukulkan.metamodel.foundation.DomainModel;
+import mx.infotec.dads.kukulkan.metamodel.foundation.ProjectConfiguration;
+
+/**
+ * TranslatorService for Database elements
+ * 
+ * @author Daniel Cortes Pichardo
+ *
+ */
+public class DataBaseTranslatorService implements TranslatorService {
+
+    @Override
+    public DomainModel translate(ProjectConfiguration pConf, Source from) {
+        
+        return null;
+    }
+
+}
