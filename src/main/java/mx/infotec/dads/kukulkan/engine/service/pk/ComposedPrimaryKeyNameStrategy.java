dfef8aa3e89a500e0a1e4391fda12530fc421495
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-09-18 01:38:55 -0500

    adding new standard
    adding new standard

--- src/main/java/mx/infotec/dads/kukulkan/engine/service/pk/ComposedPrimaryKeyNameStrategy.java
+++ src/main/java/mx/infotec/dads/kukulkan/engine/service/pk/ComposedPrimaryKeyNameStrategy.java
@@ -3,6 +3,8 @@ package mx.infotec.dads.kukulkan.engine.service.pk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.CaseFormat;
+
 import mx.infotec.dads.kukulkan.engine.model.DefaultPhysicalName;
 import mx.infotec.dads.kukulkan.engine.service.InflectorService;
 import mx.infotec.dads.kukulkan.metamodel.conventions.PhysicalName;
@@ -22,7 +24,9 @@ public class ComposedPrimaryKeyNameStrategy implements PrimaryKeyNameStrategy {
 
     @Override
     public PhysicalName getPrimaryKeyPhysicalName(Entity entity) {
-        return new DefaultPhysicalName("id" + entity.getName(), "id" + inflectorService.pluralize(entity.getName()));
+        String entityPlural = CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL,
+                inflectorService.pluralize(entity.getName()));
+        return new DefaultPhysicalName("id" + entity.getName(), "id" + entityPlural);
     }
 
 }
